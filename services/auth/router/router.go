ba9c7b14a3f4b50a517cbdb36d38066c746c8823
Mislav Perić <mislav.peric@axilis.com>
2022-08-14 21:06:20 +0200

    feat: added go auth server, kickstarted nginx cofn

---
+++ services/auth/router/router.go
@@ -0,0 +1,41 @@
+package router
+
+import (
+	"encoding/gob"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	"github.com/mislavpericaxilis/product-services/auth/authenticator"
+	"github.com/mislavpericaxilis/product-services/auth/web/callback"
+	"github.com/mislavpericaxilis/product-services/auth/web/login"
+	"github.com/mislavpericaxilis/product-services/auth/web/logout"
+	"github.com/mislavpericaxilis/product-services/auth/web/user"
+)
+
+// New registers the routes and returns the router.
+func New(auth *authenticator.Authenticator) *gin.Engine {
+	router := gin.Default()
+
+	// To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
+
+	router.Static("/public", "web/static")
+	router.LoadHTMLGlob("web/template/*")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "home.html", nil)
+	})
+	router.GET("/login", login.Handler(auth))
+	router.GET("/callback", callback.Handler(auth))
+	router.GET("/user", user.Handler)
+	router.GET("/logout", logout.Handler)
+
+	return router
+}
