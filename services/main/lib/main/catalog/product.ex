f12a893817dac00ac0ca0a76dfe5d8ea5e38f45f
Mislav Perić <mislav.peric@axilis.com>
2022-08-15 11:33:54 +0200

    feat: kickstared echo integration

---
+++ services/main/lib/main/catalog/product.ex
@@ -0,0 +1,19 @@
+defmodule Main.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :description, :string
+    field :price, :decimal
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:title, :description, :price])
+    |> validate_required([:title, :description, :price])
+  end
+end
